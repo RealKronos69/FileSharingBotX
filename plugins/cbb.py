2bea29b3f4c53a5719f148fd4a8abd98ea3ca1c3
RealKronos69 <138235386+RealKronos69@users.noreply.github.com>
2023-10-29 01:19:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz
+#(©)KronoXbotz
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>ThiS PersoN</a>\n○ Language : <code>Python3</code>\n○ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://t.me/+TEFz3qZh2zhjZjE1'>ClicK HerE</a>\n○ ᴄʜᴀɴɴᴇʟ : @Spartans_network\n○ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : @Spartans_Mainchat</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data = "close")
                     ]
                 ]
             )
